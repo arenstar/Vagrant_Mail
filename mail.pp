9199e920bb700d5796f4e0210b38956bc91a9fa9
root <root@mail.arenstar.net>
2016-03-16 13:38:47 +0000

    Initial push

---
+++ mail.pp
@@ -0,0 +1,85 @@
+node default {
+
+  package { 
+    'opensmtpd':
+      ensure => 'latest';
+  }->
+  file {
+    '/etc/smtpd.conf':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      source => "file:///vagrant/opensmtpd/smtpd.conf";
+    '/etc/maildomains':
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      content => "arenstar.net\n";
+    '/etc/pki/wildcard_arenstar.net.key':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0600',
+      source => "file:///vagrant/pki/wildcard_arenstar.net.key";
+    '/etc/pki/wildcard_arenstar.net.pem':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0600',
+      source => "file:///vagrant/pki/wildcard_arenstar.net.pem";
+    '/etc/pki/arenstar_CA_cert.pem':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0600',
+      source => "file:///vagrant/pki/arenstar_CA_cert.pem";
+  }
+
+  package { 
+    'dovecot-imapd':
+      ensure => 'latest';
+    'dovecot-antispam':
+      ensure => 'latest';
+    'dovecot-lmtpd':
+      ensure => 'latest';
+  }->
+  file {
+    '/etc/dovecot/dovecot.conf':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      source => "file:///vagrant/dovecot/dovecot.conf";
+    '/usr/bin/sa-learn-pipe.sh':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      source => "file:///vagrant/dovecot/sa-learn-pipe.sh";
+  }
+
+  package { 
+    'spampd':
+      ensure => 'latest';
+  }->
+  file {
+    '/etc/default/spamassassin':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      source => "file:///vagrant/spamassassin/default";
+  }
+
+  file {
+    ['/etc/skel/Maildir','/etc/skel/Maildir/cur','/etc/skel/Maildir/new','/etc/skel/Maildir/tmp']:
+      ensure => present;
+    ['/etc/skel/Maildir/.Drafts','/etc/skel/Maildir/.Drafts/cur','/etc/skel/Maildir/.Drafts/new','/etc/skel/Maildir/.Drafts/tmp']:
+      ensure => present;
+    ['/etc/skel/Maildir/.Sent','/etc/skel/Maildir/.Sent/cur','/etc/skel/Maildir/.Sent/new','/etc/skel/Maildir/.Sent/tmp']:
+      ensure => present;
+    ['/etc/skel/Maildir/.Trash','/etc/skel/Maildir/.Trash/cur','/etc/skel/Maildir/.Trash/new','/etc/skel/Maildir/.Trash/tmp']:
+      ensure => present;
+    ['/etc/skel/Maildir/.Templates','/etc/skel/Maildir/.Templates/cur','/etc/skel/Maildir/.Templates/new','/etc/skel/Maildir/.Templates/tmp']:
+      ensure => present;
+  }
+
+
+}
---
+++ provision_mail.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget --quiet http://apt.puppetlabs.com/puppetlabs-release-trusty.deb -O /tmp/puppetlabs-release-trusty.deb
+dpkg -i /tmp/puppetlabs-release-trusty.deb
+apt-get update
+apt-get install -y puppet-common puppet ldapscripts git
+sed -i '/templatedir/d' /etc/puppet/puppet.conf
+
+puppet module install garethr-docker
+puppet module install jproyo-dovecot
+puppet module install wyrie-spamassassin
