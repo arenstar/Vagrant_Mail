503e7ee29e3953059e19f1318b6e269030a6c7b9
root <root@mail.arenstar.net>
2016-04-02 11:02:29 +0000

    Adding augeas

--- provision_mail.sh
+++ provision_mail.sh
@@ -11,3 +11,5 @@ puppet module install jproyo-dovecot
 puppet module install wyrie-spamassassin
 puppet module install puppetlabs-firewall
 puppet module install dhoppe-fail2ban
+puppet module install herculesteam/augeasproviders_base
+puppet module install jfryman-nginx
