4f2f402f01402e6c805f4ec1eda08b6f8eb4d279
root <root@arenstar.net>
2016-04-19 11:30:00 +0000

    Dockerized + LDAP support

--- puppet/setup.pp
+++ puppet/setup.pp
@@ -1,128 +1,132 @@
 node default {
 
-  package { 
-    'opensmtpd':
-      ensure => 'latest';
-  }->
-  file {
-    '/etc/smtpd.conf':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      notify => Service['opensmtpd'],
-      source => "file:///vagrant/puppet/opensmtpd/smtpd.conf";
-    '/etc/maildomains':
-      ensure  => present,
-      owner   => 'root',
-      group   => 'root',
-      content => "arenstar.net\n";
-    '/etc/blacklist-recipients':
-      ensure  => present,
-      owner   => 'root',
-      group   => 'root',
-      content => "\n";
-  }->
-  service {
-    'opensmtpd':
-      ensure    => running,
-      enable    => true,
-      hasstatus => false,
-      pattern   => 'smtpd',
-      require   => File['/etc/smtpd.conf'];
+  #package {
+  #  'spampd':
+  #    ensure => 'latest';
+  #}->
+  #file {
+  #  '/etc/default/spamassassin':
+  #    ensure => present,
+  #    owner  => 'root',
+  #    group  => 'root',
+  #    source => "file:///vagrant/puppet/files/spamassassin/default";
+  #}->
+  #service {
+  #  'spampd':
+  #    ensure => running,
+  #    enable => true,
+  #}
+
+
+  include firewall_setup
+  include docker
+
+  class { 'fail2ban': }
+
+  docker::run { 'znc':
+    image              => 'jimeh/znc',
+    memory_limit       => '16m',
+    ports              => ['6667:6667'],
+    volumes            => ['/vagrant/puppet/files/znc/znc.conf:/znc-data/configs/znc.conf:rw'],
+    restart_service    => true,
   }
 
-  mailalias {
-    "root":
-      ensure    => present,
-      recipient => "test",
-      provider  => augeas,
-  }
- 
-  package {
-    'dovecot-imapd':
-      ensure => 'latest';
-    'dovecot-antispam':
-      ensure => 'latest';
-    'dovecot-lmtpd':
-      ensure => 'latest';
+  docker::image { 'arenstar/opensmtpd':
+    docker_dir => '/vagrant/opensmtpd'
   }->
-  file {
-    '/etc/dovecot/dovecot.conf':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      notify => Service['dovecot'],
-      source => "file:///vagrant/puppet/dovecot/dovecot.conf";
-    '/usr/bin/sa-learn-pipe.sh':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      source => "file:///vagrant/puppet/dovecot/sa-learn-pipe.sh";
-  }
-  service {
-    'dovecot':
-      enable    => true,
-      ensure    => running,
-      hasstatus => true,
-      require   => File['/etc/dovecot/dovecot.conf'];
+  docker::run { 'opensmtpd':
+    image              => 'arenstar/opensmtpd',
+    memory_limit       => '64m',
+    ports              => ['25:25','587:587'],
+    hostname           => 'mail.arenstar.net',
+    hostentries        => ['ldap.arenstar.net:172.17.0.1'],
+    volumes            => [
+       '/etc/mailname:/etc/mailname:ro',
+       '/vagrant/puppet/files/opensmtpd/smtpd.conf:/etc/smtpd.conf:ro',
+       '/vagrant/puppet/files/opensmtpd/smtpd-ldap.conf:/etc/smtpd-ldap.conf:ro',
+       '/etc/pki/wildcard_arenstar.net.pem:/etc/wildcard_arenstar.net.pem:ro',
+       '/etc/pki/wildcard_arenstar.net.key:/etc/wildcard_arenstar.net.key:ro',
+    ],
+    restart_service    => true,
   }
 
-  package {
-    'spampd':
-      ensure => 'latest';
-  }->
-  file {
-    '/etc/default/spamassassin':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      source => "file:///vagrant/puppet/spamassassin/default";
-  }->
-  service {
-    'spampd':
-      ensure => running,
-      enable => true,
+  docker::run { 'registrator':
+    image              => 'gliderlabs/registrator',
+    memory_limit       => '16m',
+    net                => 'host',
+    command            => 'consul://localhost:8500',
+    volumes            => [
+       '/var/run/docker.sock:/tmp/docker.sock:ro',
+    ],
   }
 
-  package{ 
-    'python-pam':
-      ensure => present;
-    'python-pampy':
-      ensure => present;
-    'libpam-python':
-      ensure => present;
+  docker::run { 'consul':
+    image              => 'progrium/consul',
+    memory_limit       => '16m',
+    ports              => ['8400:8400','8500:8500','8600:53/udp'],
+    hostname           => 'consul.arenstar.net',
+    command            => '-server -bootstrap -ui-dir /ui',
+    restart_service    => true,
+  }
+
+  docker::run { 'amavis':
+    image              => 'flyhard/amavis',
+    memory_limit       => '16m',
+    ports              => ['10024:10024'],
+    hostentries        => ['consul.arenstar.net:172.17.0.1'],
+    restart_service    => true,
+  }
+
+  docker::image { 'arenstar/dovecot':
+    docker_dir => '/vagrant/dovecot'
   }->
-  file { 
-    '/lib/security/emailuser.py':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      source => "file:///vagrant/puppet/files/emailuser.py";
-    '/etc/pam.d/smtpd':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      source => "file:///vagrant/puppet/files/pam-smtpd";
-    '/etc/pam.d/dovecot':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      source => "file:///vagrant/puppet/files/pam-smtpd";
+  docker::run { 'dovecot':
+    image              => 'arenstar/dovecot',
+    memory_limit       => '32m',
+    ports              => ['24:24','993:993'],
+    hostentries        => ['ldap.arenstar.net:172.17.0.1'],
+    restart_service    => true,
+    volumes            => [
+       '/vagrant/puppet/files/dovecot/dovecot.conf:/etc/dovecot/dovecot.conf:ro',
+       '/vagrant/puppet/files/dovecot/dovecot-ldap.conf.ext:/etc/dovecot/dovecot-ldap.conf.ext:ro',
+       '/vagrant/puppet/files/dovecot/sa-learn-pipe.sh:/usr/bin/sa-learn-pipe.sh:ro',
+       '/etc/pki/wildcard_arenstar.net.pem:/etc/pki/ssl_cert.pem:ro',
+       '/etc/pki/wildcard_arenstar.net.key:/etc/pki/ssl_key.pem:ro',
+    ],
+  }
+
+  docker::run { 'openldap':
+    image              => 'osixia/openldap',
+    memory_limit       => '32m',
+    ports              => ['389:389'],
+    hostname           => 'ldap.arenstar.net',
+    restart_service    => true,
+    env                => [
+       'LDAP_ORGANISATION=arenstar',
+       'LDAP_DOMAIN=arenstar.net',
+       'LDAP_ADMIN_PASSWORD=password',
+       'LDAP_CONFIG_PASSWORD=password',
+       'LDAP_READONLY_USER=true',
+       'LDAP_READONLY_USER_PASSWORD=password',
+       'LDAP_TLS=False',
+       'LDAP_REMOVE_CONFIG_AFTER_SETUP=true',
+    ],
+  }
+
+  exec { "ldap-add-users":
+    command => "/usr/bin/ldapadd -x -D 'cn=admin,dc=arenstar,dc=net' -w password -f /vagrant/puppet/files/openldap/add_users/add_users.ldif",
+    unless  => "/usr/bin/ldapsearch -x -D 'cn=admin,dc=arenstar,dc=net' -w password -b \"uid=test,ou=people,dc=arenstar,dc=net\" \"uid=test\"",
+  }
+  exec { "ldap-default-policy":
+    command => "/usr/bin/ldapadd -x -D 'cn=admin,dc=arenstar,dc=net' -w password -f /vagrant/puppet/files/openldap/default-policy.ldif",
+    unless  => "/usr/bin/ldapsearch -x -D 'cn=admin,dc=arenstar,dc=net' -w password -b \"cn=default,ou=policies,dc=arenstar,dc=net\" \"cn=default\"",
+  }
+  exec { "ldap-email":
+    command => "/usr/bin/ldapadd -x -D 'cn=admin,dc=arenstar,dc=net' -w password -f /vagrant/puppet/files/openldap/email_setup/email_setup.ldif",
+    unless  => "/usr/bin/ldapsearch -x -D 'cn=admin,dc=arenstar,dc=net' -w password -b \"cn=stuff@arenstar.net,ou=aliases,dc=arenstar,dc=net\" \"cn=stuff@arenstar.net\"",
   }
 
   file {
-    ['/etc/skel/Maildir','/etc/skel/Maildir/cur','/etc/skel/Maildir/new','/etc/skel/Maildir/tmp']:
-      ensure => directory;
-    ['/etc/skel/Maildir/.Drafts','/etc/skel/Maildir/.Drafts/cur','/etc/skel/Maildir/.Drafts/new','/etc/skel/Maildir/.Drafts/tmp']:
-      ensure => directory;
-    ['/etc/skel/Maildir/.Sent','/etc/skel/Maildir/.Sent/cur','/etc/skel/Maildir/.Sent/new','/etc/skel/Maildir/.Sent/tmp']:
-      ensure => directory;
-    ['/etc/skel/Maildir/.Trash','/etc/skel/Maildir/.Trash/cur','/etc/skel/Maildir/.Trash/new','/etc/skel/Maildir/.Trash/tmp']:
-      ensure => directory;
-    ['/etc/skel/Maildir/.Templates','/etc/skel/Maildir/.Templates/cur','/etc/skel/Maildir/.Templates/new','/etc/skel/Maildir/.Templates/tmp']:
-      ensure => directory;
-    '/etc/pki':
-      ensure => directory;
     '/etc/pki/wildcard_arenstar.net.key':
       ensure => present,
       owner  => 'root',
@@ -141,66 +145,137 @@ node default {
       group  => 'root',
       mode   => '0600',
       source => "file:///vagrant/pki/arenstar_CA_cert.pem";
+    '/etc/mailname':
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      content => "arenstar.net\n";
   }
+}
 
-  group { 'mail':
-    ensure => 'present',
-  }->
-  user { 'test':
-    ensure           => 'present',
-    home             => '/home/test',
-    managehome       => true,
-    password         => '$6$STFoC3Nx$0YurTwTvQG3f2otaFXGxAIo3DaVlgxJhbwh5CGAdQLV9Bh/LrE2mvvAHIZJevCxlWm2LNQVNprsHp5RZEYUB2/',
-    shell            => '/bin/bash',
-    groups           => 'mail',
-  }
+class firewall_setup {
 
-  class { 'fail2ban': }
+    class { 'firewall': }
 
-  # basic firewall declarations
-  class { 'firewall': }
-  firewall { '000 accept all icmp':
-    proto   => 'icmp',
-    action  => 'accept',
-  }->
-  firewall { '001 accept all to lo interface':
-    proto   => 'all',
-    iniface => 'lo',
-    action  => 'accept',
-  }->
-  firewall { '003 accept related established rules':
-    proto   => 'all',
-    state   => ['RELATED', 'ESTABLISHED'],
-    action  => 'accept',
-  }->
-  firewall { '990 Allow INPUT SMTP':
-    chain   => 'INPUT',
-    dport   => ['25','587'],
-    proto   => 'tcp',
-    action  => 'accept',
-  }->
-  firewall { '990 Allow INPUT IMAP':
-    chain   => 'INPUT',
-    dport   => ['143','993'],
-    proto   => 'tcp',
-    action  => 'accept',
-  }->
-  firewall { '990 Allow INPUT SSH EXTERNAL':
-    chain   => 'INPUT',
-    dport    => '22',
-    proto   => 'tcp',
-    action  => 'accept',
-  }->
-  firewall { '995 Allow ESTABLISHED INPUT SSH':
-    chain   => 'INPUT',
-    dport   => '22',
-    proto   => 'tcp',
-    state   => 'ESTABLISHED',
-    action  => 'accept',
-  }->
-  firewall { '996 drop all':
-    proto   => 'all',
-    action  => 'drop',
-    before  => undef,
-  }
+    # basic firewall declarations
+    # Ignore docker dynamic rules
+    firewallchain { 'DOCKER:nat:IPv4':
+        ensure => present,
+        purge  => true,
+        ignore => [
+            '-o docker0',
+            '-i docker0',
+            '172.17.',
+        ],
+    }->
+    firewallchain { 'POSTROUTING:nat:IPv4':
+        ensure => present,
+        purge  => true,
+        ignore => [
+            '-o docker0',
+            '-i docker0',
+            '172.17.',
+        ],
+    }->
+    firewallchain { 'DOCKER:filter:IPv4':
+        ensure => present,
+        purge  => true,
+        ignore => [
+            '-o docker0',
+            '-i docker0',
+            '172.17.',
+        ],
+    }->
+    firewallchain { 'INPUT:filter:IPv4':
+        ensure => present,
+        purge  => true,
+        policy => 'accept',
+        ignore => [
+            '-o docker0',
+            '-i docker0',
+            '172.17.',
+        ],
+    }->
+    firewallchain { 'FORWARD:filter:IPv4':
+        ensure => present,
+        purge  => true,
+        policy => 'accept',
+        ignore => [
+            '-o docker0',
+            '-i docker0',
+            '172.17.',
+        ],
+    }
+    firewallchain { 'OUTPUT:filter:IPv4':
+        ensure => present,
+        purge  => true,
+        policy => 'accept',
+        ignore => [
+            '-o docker0',
+            '-i docker0',
+            '172.17.',
+        ],
+    }
+
+    firewall { '000 accept all icmp':
+      proto   => 'icmp',
+      action  => 'accept',
+    }->
+    firewall { '001 accept all to lo interface':
+      proto   => 'all',
+      iniface => 'lo',
+      action  => 'accept',
+    }->
+    firewall { '003 accept related established rules':
+      proto   => 'all',
+      state   => ['RELATED', 'ESTABLISHED'],
+      action  => 'accept',
+    }->
+    firewall { '004 accept all to docker interfaces':
+      proto   => 'all',
+      iniface => 'docker0',
+      action  => 'accept',
+    }->
+    firewall { '990 Allow INPUT SMTP':
+      chain   => 'INPUT',
+      dport   => ['25','587'],
+      proto   => 'tcp',
+      action  => 'accept',
+    }->
+    firewall { '990 Allow INPUT IMAP':
+      chain   => 'INPUT',
+      dport   => ['143','993'],
+      proto   => 'tcp',
+      action  => 'accept',
+    }->
+    firewall { '990 Allow INPUT IRC':
+      chain   => 'INPUT',
+      dport   => ['6667'],
+      proto   => 'tcp',
+      action  => 'accept',
+    }->
+    firewall { '990 Allow INPUT HTTP':
+      chain   => 'INPUT',
+      dport   => ['80','443'],
+      proto   => 'tcp',
+      action  => 'accept',
+    }->
+    firewall { '990 Allow INPUT SSH EXTERNAL':
+      chain   => 'INPUT',
+      dport    => '22',
+      proto   => 'tcp',
+      action  => 'accept',
+    }->
+    firewall { '995 Allow ESTABLISHED INPUT SSH':
+      chain   => 'INPUT',
+      dport   => '22',
+      proto   => 'tcp',
+      state   => 'ESTABLISHED',
+      action  => 'accept',
+    }->
+    firewall { '996 drop all':
+      proto   => 'all',
+      action  => 'drop',
+      before  => undef,
+    }
 }
